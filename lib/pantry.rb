c8b635e76b000f20d35975476ceee9eba938be50
Ryan Laleh <laleh21@yahoo.com>
2020-06-03 10:27:22 -0600

    test if it can check stock test and passes

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,7 +1,11 @@
 class Pantry
   attr_reader :stock
-  
+
   def initialize()
     @stock = {}
+  end
+
+  def stock_check(ingredient)
+    @stock.count(ingredient)
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,10 +7,16 @@ require './lib/pantry'
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists_and_has_attribute
     assert_instance_of Pantry, @pantry
     assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_checks_stock
+    assert_equal 0, @pantry.stock_check(@ingredient1)
   end
 end
