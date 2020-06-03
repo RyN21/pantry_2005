a0fe8c6cd50b6ef928b41de037eeddfb658d2c6a
Ryan Laleh <laleh21@yahoo.com>
2020-06-03 11:41:24 -0600

    Itcan find ingredients in cookbook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,11 +1,21 @@
 class CookBook
-  attr_reader :recipes
-  
+  attr_reader :recipes, :ingredients
+
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+  end
+
+  def ingredients
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        @ingredients << ingredient.name
+      end
+    end
+    @ingredients.uniq
   end
 end
